d3758d5c7c2d328bd075157421e763b33bcdfb19
Charles Feng <charles@charles.io>
2024-09-18 14:21:13 -0600

    chore: add license (#8)

--- test/engine/findMany.spec.ts
+++ test/engine/findMany.spec.ts
@@ -1,9 +1,25 @@
+/**
+ * Copyright (C) 2024 Acro Data Solutions, Inc.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { describe, it, expect, beforeEach } from "vitest";
-import { Action } from "@acro-sdk/common-store";
 import dotenv from "dotenv";
 
-import { OpenSearchAction, OpenSearchEngine } from "../../src";
-import { ACTION_TEMPLATE, OPENSEARCH_ACTION_TEMPLATE } from "../mock/actions";
+import { OpenSearchEngine } from "../../src";
+import { OPENSEARCH_ACTION_TEMPLATE } from "../mock/actions";
 
 describe("OpenSearchEngine.findMany", () => {
   let engine: OpenSearchEngine;
